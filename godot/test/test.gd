d46156fb4fa503dbd45f53eb729b325f0781e4b7
SimonPiCarter <xxx@gmail.com>
2024-02-22 21:21:27 +0100

    added frames

--- godot/test/test.gd
+++ godot/test/test.gd
@@ -9,6 +9,10 @@ func _ready():
 	entity_drawer.set_time_step(0.1)
 
 	frames_library.addFrame("blue", preload("res://godot/frames/jams/blue.tres"), Vector2(0,0), false)
+	frames_library.addFrame("green", preload("res://godot/frames/jams/green.tres"), Vector2(0,0), false)
+	frames_library.addFrame("pink", preload("res://godot/frames/jams/pink.tres"), Vector2(0,0), false)
+	frames_library.addFrame("red", preload("res://godot/frames/jams/red.tres"), Vector2(0,0), false)
+	frames_library.addFrame("yellow", preload("res://godot/frames/jams/yellow.tres"), Vector2(0,0), false)
 
 	line_manager.setEntityDrawer(entity_drawer)
 	line_manager.setFramesLibrary(frames_library)
